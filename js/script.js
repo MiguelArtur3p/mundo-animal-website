4de782f2364ddff77b79689904351f09fcd73652
Miguel <miguel.3tp@hotmail.com>
2024-01-18 09:31:05 -0300

    init modal

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -16,13 +16,15 @@ const tabNav = new TabNav(
   '[data-tab="menu"] li',
   '[data-tab="content"] section'
 );
+const modal = new Modal();
+
 scrollSuave.init();
 accordion.init();
 tabNav.init();
+modal.init();
 
 initAnimacaoScroll();
 
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
